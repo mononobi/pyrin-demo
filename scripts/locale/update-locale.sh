324f6a38112739302dd476b078a6114f2ceddd49
mono <mohamadnobakht@gmail.com>
2020-06-12 20:40:33 +0430

    Delete not needed scripts

--- scripts/locale/update-locale.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-pybabel extract -o ../../src/demo/locale/messages.pot ../../src/demo/
-pybabel update -i ../../src/demo/locale/messages.pot -d ../../src/demo/locale/
-pybabel compile -d ../../src/demo/locale/
