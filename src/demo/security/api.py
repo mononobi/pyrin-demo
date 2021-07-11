d2f6aaca10e3865c4fb080c39f84179e4aa9e172
mono <mohamadnobakht@gmail.com>
2021-07-12 03:32:54 +0430

    Revise docstrings

--- src/demo/security/api.py
+++ src/demo/security/api.py
@@ -3,22 +3,39 @@
 security api module.
 """
 
-from pyrin.api.router.decorators import api
-from pyrin.core.enumerations import HTTPMethodEnum
+from pyrin.api.router.decorators import post
 
 import demo.security.services as security_services
 
 
-@api('/auth/login', methods=HTTPMethodEnum.POST, authenticated=False)
+@post('/auth/login', authenticated=False)
 def login(username, password, **options):
     """
-    logs in the provided user and gets a valid token.
-
-    :param str username: username.
-    :param str password: password.
-
-    :returns: a valid token.
-    :rtype: str
+    logs in the provided user and gets a valid access and refresh token.
+    ---
+    parameters:
+      - name: username
+        type: string
+        description: username
+      - name: password
+        type: string
+        format: password
+        description: password
+    responses:
+      201:
+        description: user has successfully logged in
+        schema:
+          properties:
+            access_token:
+              type: string
+              description: access token
+            refresh_token:
+              type: string
+              description: refresh token
+      401:
+        description: authentication failed
+      403:
+        description: user is not active
     """
 
     return security_services.login(username, password, **options)
